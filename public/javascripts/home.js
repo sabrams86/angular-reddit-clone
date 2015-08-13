4d2f5ac83451bb1a2153b0235b3593ce43655af4
Steven Abrams <steven.abrams86@gmail.com>
2015-08-13 07:32:26 -0600

    form added

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,5 +1,12 @@
 var app = angular.module("rclone", []);
 
+app.controller("NavbarController", function ($scope) {
+  $scope.newPost = false;
+  $scope.showPostForm = function () {
+    $scope.newPost = !$scope.newPost;
+  }
+})
+
 app.controller("ContentController", function ($scope) {
-  $scope.name = "Steve";
+
 })
