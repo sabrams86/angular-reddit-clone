78b083c2a8fb8e6fbb4a9b295ecf320a682d3ce5
Steven Abrams <steven.abrams86@gmail.com>
2015-08-17 13:24:38 -0600

    comments working, errors working

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -12,20 +12,45 @@ var posts = [
     comments: [{name: "Steve", content: "This is a comment"}, {name: "Guy", content: "What are you talking about!"}],
   },
   {
-    title: "New Dinosaur Discovered",
+    title: "Dino-Myte",
     author: "Guy",
     url: "http://www.margothovley.com/wp-content/uploads/2012/03/homework-class-test-school-of-fail-the-thesaurus-was-the-mightiest-beast-of-the-dinosaur-kingdom.jpg",
     description: "Breaking news!  It's a new dinosaur!",
     date: Date.now(),
-    votes: 4,
+    votes: 1,
     comments: [{name: "Steve", content: "Angular Rocks"}, {name: "Dinoman", content: "Hah, that's hilarious"}],
-  }
+  },
+  {
+    title: "Silicon Valley",
+    author: "Pied Piper",
+    url: "https://media2.giphy.com/media/dbHS31nm21uAU/200.gif",
+    description: "Welcome and hey",
+    date: Date.now()-300000000,
+    votes: -4,
+    comments: [{name: "Steve", content: "Angular Rocks"}],
+  },
+  {
+    title: "New Dino",
+    author: "Guy",
+    url: "http://www.margothovley.com/wp-content/uploads/2012/03/homework-class-test-school-of-fail-the-thesaurus-was-the-mightiest-beast-of-the-dinosaur-kingdom.jpg",
+    description: "Breaking news!  It's a new dinosaur!",
+    date: Date.now()-509900000,
+    votes: 0,
+    comments: [],
+  },
+  {
+    title: "New Dinosaur Discovered",
+    author: "Guy",
+    url: "http://www.margothovley.com/wp-content/uploads/2012/03/homework-class-test-school-of-fail-the-thesaurus-was-the-mightiest-beast-of-the-dinosaur-kingdom.jpg",
+    description: "Breaking news!  It's a new dinosaur!",
+    date: Date.now()-100000000,
+    votes: 4,
+    comments: [{name: "Steve", content: "Angular Rocks"}, {name: "Dinoman", content: "Hah, that's hilarious"}, {name: "Dinoman", content: "But really, it's great"}],
+  },
 ];
 
 
 app.controller("NavbarController", function ($scope) {
-  var vm = this;
-  vm.time = new Date();
   $scope.sortOrder = "Sort By Votes"
   $scope.newPost = false;
   $scope.showComments = false;
@@ -48,6 +73,7 @@ app.controller("NavbarController", function ($scope) {
     $scope.author = '';
     $scope.url = '';
     $scope.description = '';
+    $scope.userForm.$setPristine();
   }
   $scope.postComment = function () {
     var comment = {};
@@ -58,6 +84,7 @@ app.controller("NavbarController", function ($scope) {
     this.comments.name = '';
     this.comments.content = '';
     this.showComments = true;
+    $scope.commentForm.$setPristine();
   }
   $scope.orderVotes = function () {
     $scope.sortOrder = "Sort By Votes";
@@ -88,11 +115,6 @@ app.controller("NavbarController", function ($scope) {
   }
   $scope.commentFormShow = function () {
     this.showCommentForm = !this.showCommentForm;
-  }
-  $scope.submitForm = function (isValid) {
-    if (isValid) {
-      alert('our form is amazing');
-    }
   }
 })
 
